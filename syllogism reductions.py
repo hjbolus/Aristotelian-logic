85ba396a4a3ef65b07baa51d2e10e013ba9316a5
hjbolus <141349319+hjbolus@users.noreply.github.com>
2025-07-19 09:06:59 -0500

    Update syllogism reductions.py

--- syllogism reductions.py
+++ syllogism reductions.py
@@ -27,22 +27,26 @@ def reduce_syllogism(syllogism, chain: list, end_points: list):
                     return chain
     return chain
 
+def all_syllogisms_generator():
+    # Yields all syllogisms
+    for figure in ['1','2','3','4']:
+        for quantifiers in product(['a','e','i','o'], repeat=3):
+            yield Syllogism.from_terms_figure_and_quantifiers('S','M','P', figure, quantifiers)
+
 if __name__ == "__main__":
     valid_chains = []
     invalid_chains = []
-    for figure in ['1','2','3','4']:
-        for quantifiers in product(['a','e','i','o'], repeat=3):
-            syllogism =  Syllogism.from_terms_figure_and_quantifiers('S','M','P', figure, quantifiers)
-            if syllogism.mood in end_points:
-                result = [(syllogism.mood, True)]
-            else:
-                result = reduce_syllogism(syllogism, [], end_points)
-            if result[-1][1] == True:
-                valid_chains.append([i[0] for i in result])
-            else:
-                result[-1] = (result[-1][0], False)
-                invalid_chains.append([i[0] for i in result])
-            print(syllogism.mood, result[-1][1])
+    for syllogism in all_syllogisms_generator:
+        if syllogism.mood in end_points:
+            result = [(syllogism.mood, True)]
+        else:
+            result = reduce_syllogism(syllogism, [], end_points)
+        if result[-1][1] == True:
+            valid_chains.append([i[0] for i in result])
+        else:
+            result[-1] = (result[-1][0], False)
+            invalid_chains.append([i[0] for i in result])
+        print(syllogism.mood, result[-1][1])
 
 # ['barbara']
 # ['barbari']
